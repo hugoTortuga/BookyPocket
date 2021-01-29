3c191ebd1a272ed10208b8cdc002b51db3d2593a
El Tortuga <hugo.malglaive@gmail.com>
2021-01-29 19:11:35 +0100

    recherche de bouquin v0 fonctionnel

--- Appli/Dev/BookyPocket/app/src/main/java/android/cnam/bookypocket/API/API_GoogleBooks.java
+++ Appli/Dev/BookyPocket/app/src/main/java/android/cnam/bookypocket/API/API_GoogleBooks.java
@@ -2,8 +2,10 @@ package android.cnam.bookypocket.API;
 
 import android.cnam.bookypocket.Model.Book;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,14 +14,62 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
+import com.google.gson.*;
 
 public class API_GoogleBooks {
 
-    private List<Book> JSON_Decryptor(JSONObject json){
+    private static List<Book> JSON_Decryptor(JSONObject json) throws JSONException {
+        List<Book> books = new ArrayList<>();
+        JSONArray items = json.getJSONArray("items");
+        int nbitems = json.getInt("totalItems");
+        for(int i = 0; i < 30; i++){
+            JSONObject item = items.getJSONObject(i);
+            JSONObject volumeInfo = item.getJSONObject("volumeInfo");
+            String title = volumeInfo.getString("title");
 
-        // TODO
-        return null;
+            Book b1 = new Book();
+            b1.setTitle(title + " " + i);
+            books.add(b1);
+        }
+
+
+
+        return books;
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    private static String ReadStringJSON(String url) throws IOException{
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            return jsonText;
+        } finally {
+            is.close();
+        }
+    }
+
+    private static JSONObject ReadJsonFromUrl(String url) throws IOException, JSONException {
+        return new JSONObject(ReadStringJSON(url));
+    }
+
+    public static List<Book> Request(String keyword) throws Exception {
+        if(keyword == null || keyword.trim().equals(""))
+            throw new Exception("Le champ de recherche est vide");
+
+        keyword = keyword.replace(' ', '+');
+        JSONObject json = API_GoogleBooks.ReadJsonFromUrl("https://www.googleapis.com/books/v1/volumes?q=" + keyword +  " &maxResults=30");
+        return JSON_Decryptor(json);
     }
 
 
--- Appli/Dev/BookyPocket/app/src/main/java/android/cnam/bookypocket/BookSearchActivity.java
+++ Appli/Dev/BookyPocket/app/src/main/java/android/cnam/bookypocket/BookSearchActivity.java
@@ -3,37 +3,39 @@ package android.cnam.bookypocket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ListActivity;
+import android.cnam.bookypocket.API.API_GoogleBooks;
 import android.cnam.bookypocket.DBManager.Session;
+import android.cnam.bookypocket.Model.Book;
 import android.cnam.bookypocket.ui.ReadingsListAdapter;
 import android.cnam.bookypocket.utils.Alert;
+import android.cnam.bookypocket.utils.ChangeActivity;
+import android.content.Context;
 import android.graphics.drawable.AdaptiveIconDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SearchView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class BookSearchActivity extends AppCompatActivity {
 
     private SearchView searchView;
     private ImageButton itemImageButton;
 
-
-
-    //image button will change
-    //private Drawable addIcon = getResources().getDrawable( R.drawable.plus );
-    //private Drawable removeIcon = getResources().getDrawable( R.drawable.minus );
-
-
     private ArrayList<String> titleList = new ArrayList<>(
-            Arrays.asList("Buenos Aires", "Córdoba", "La Plata","Buenos Aires", "Córdoba", "La Plata",
-                    "Buenos Aires", "Córdoba", "La Plata","Buenos Aires", "Córdoba", "La Plata",
-                    "Buenos Aires", "Córdoba", "La Plata","Buenos Aires", "Córdoba", "La Plata"));
+            Arrays.asList("Liste des ouvrages"));
 
     private ListView found_list;
 
@@ -46,18 +48,26 @@ public class BookSearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book_search);
 
-
+        searchView = (SearchView) findViewById(R.id.search_book_button);
         found_list = (ListView) findViewById(R.id.found_list);
         adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titleList);
         found_list.setAdapter(adapter);
-        Alert.ShowDialog(this,"found list",""+found_list.getCount());
     }
 
     public void searchBook(View view){
         try{
             if(Session.isUserConnectedToInternet()){
-                Alert.ShowDialog(this, "Recherche",
-                        "x résultats");
+                if(searchView.getQuery() == null || searchView.getQuery().equals(""))
+                    Alert.ShowDialog(this, "0 résultat","Le champ de recherche est vide");
+
+                else{
+                    String keyword = searchView.getQuery().toString();
+                    MyAsyncTask task = new MyAsyncTask(this,keyword);
+                    task.execute();
+                }
+
+
+
             }
             else{
                 Alert.ShowDialog(this, "Pas de connexion internet",
@@ -68,6 +78,58 @@ public class BookSearchActivity extends AppCompatActivity {
             Alert.ShowError(this, "Le recherche de livre a provoqué une erreur",
                     "" + ex);
         }
+    }
+
+    public void GoHome(View view) {
+        ChangeActivity.ChangeActivity(this, MainActivity.class);
+    }
+
+    public class MyAsyncTask extends AsyncTask<Void, Void, String> {
+
+        private List<Book> books;
+        private BookSearchActivity it;
+        private String keyword;
+
+        public MyAsyncTask(BookSearchActivity _it, String _keyword) {
+            super();
+            it = _it;
+            keyword = _keyword;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            Alert.ShowError(it, "Début recherche", "" + keyword);
+            books = new ArrayList<Book>();
+
+            try {
+                books = API_GoogleBooks.Request(keyword);
+            } catch (Exception ex) {
+                Log.e("FATAL","fatal 55555555" + ex);
+                Alert.ShowError(it, "Erreur lors de l'appel à l'api Google Books", "" + ex);
+            }
+
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            try {
+                List<String> livres = new ArrayList<>();
+                for (Book b: books) {
+                    livres.add(b.getTitle());
+                }
+
+                //found_list = (ListView) findViewById(R.id.found_list);
+                adapter=new ArrayAdapter<String>(it, android.R.layout.simple_list_item_1, livres);
+                found_list.setAdapter(adapter);
+            } catch (Exception ex) {
+                Alert.ShowError(it, "Erreur lors de l'affichage des données", "" + ex);
+            }
+
+
+        }
+
     }
 
 }
